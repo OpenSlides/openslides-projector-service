57153ea0acb5b04ba665ae358a2a504c406e950b
Bastian Rihm <brihm@intevation.de>
2025-07-10 16:26:14 +0200

    Move current list of speakers logic to viewmodel

--- pkg/projector/slide/current_list_of_speakers.go
+++ pkg/projector/slide/current_list_of_speakers.go
@@ -3,8 +3,6 @@ package slide
 import (
 	"context"
 	"fmt"
-	"sort"
-	"strings"
 
 	"github.com/OpenSlides/openslides-projector-service/pkg/viewmodels"
 )
@@ -24,70 +22,23 @@ func CurrentListOfSpeakersSlideHandler(ctx context.Context, req *projectionReque
 		return nil, nil
 	}
 
-	lQ := req.Fetch.ListOfSpeakers(*losID)
-	los, err := lQ.
-		Preload(lQ.SpeakerList().MeetingUser().StructureLevelList()).
-		Preload(lQ.SpeakerList().MeetingUser().User()).First(ctx)
+	speakers, err := viewmodels.ListOfSpeakers_CategorizedLists(ctx, req.Fetch, *losID)
 	if err != nil {
-		return nil, fmt.Errorf("could not load speakers: %w", err)
+		return nil, fmt.Errorf("could not categorize speakers %w", err)
 	}
 
-	type speakerListItem struct {
-		Name   string
-		Weight int
+	if len(speakers.WaitingInterposedQuestions) == 0 &&
+		len(speakers.WaitingSpeakers) == 0 &&
+		speakers.CurrentSpeaker == nil &&
+		speakers.CurrentInterposedQuestion == nil {
+		return nil, nil
 	}
-	waitingSpeakers := []speakerListItem{}
-	interposedQuestions := []speakerListItem{}
-	var currentSpeaker *speakerListItem
-	var currentInterposedQuestion *speakerListItem
-	for _, speaker := range los.SpeakerList {
-		name := ""
-		if meetingUser, isSet := speaker.MeetingUser.Value(); isSet {
-			user := meetingUser.User
-			name = viewmodels.User_ShortName(user)
-			if len(meetingUser.StructureLevelList) != 0 {
-				structureLevelNames := []string{}
-				for _, sl := range meetingUser.StructureLevelList {
-					structureLevelNames = append(structureLevelNames, sl.Name)
-				}
-
-				name = fmt.Sprintf("%s (%s)", name, strings.Join(structureLevelNames, ", "))
-			}
-		}
-
-		item := speakerListItem{
-			Name:   name,
-			Weight: speaker.Weight,
-		}
-
-		if (speaker.BeginTime == 0) && speaker.EndTime == 0 {
-			if speaker.SpeechState == "interposed_question" {
-				interposedQuestions = append(interposedQuestions, item)
-			} else {
-				waitingSpeakers = append(waitingSpeakers, item)
-			}
-		} else if speaker.EndTime == 0 {
-			if speaker.SpeechState == "interposed_question" {
-				currentInterposedQuestion = &item
-			} else {
-				currentSpeaker = &item
-			}
-		}
-	}
-
-	sort.Slice(waitingSpeakers, func(i, j int) bool {
-		return waitingSpeakers[i].Weight < waitingSpeakers[j].Weight
-	})
-
-	sort.Slice(interposedQuestions, func(i, j int) bool {
-		return interposedQuestions[i].Weight < interposedQuestions[j].Weight
-	})
 
 	return map[string]any{
-		"CurrentSpeaker":            currentSpeaker,
-		"Speakers":                  waitingSpeakers,
-		"InterposedQuestions":       interposedQuestions,
-		"CurrentInterposedQuestion": currentInterposedQuestion,
+		"CurrentSpeaker":            speakers.CurrentSpeaker,
+		"Speakers":                  speakers.WaitingSpeakers,
+		"InterposedQuestions":       speakers.WaitingInterposedQuestions,
+		"CurrentInterposedQuestion": speakers.CurrentInterposedQuestion,
 		"Overlay":                   req.Projection.Stable,
 	}, nil
 }
--- pkg/viewmodels/list_of_speakers.go
+++ pkg/viewmodels/list_of_speakers.go
@@ -2,6 +2,9 @@ package viewmodels
 
 import (
 	"context"
+	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/OpenSlides/openslides-go/datastore/dsmodels"
 )
@@ -22,4 +25,92 @@ func ListOfSpeakers_CurrentSpeaker(ctx context.Context, los *dsmodels.ListOfSpea
 	}
 
 	return currentSpeaker, nil
+}
+
+type SpeakerListItem struct {
+	Name           string
+	Weight         int
+	IsSpeaking     bool
+	IsContribution bool
+	IsPointOfOrder bool
+	IsIntervention bool
+}
+
+type ListOfSpeakersLists struct {
+	CurrentSpeaker             *SpeakerListItem
+	WaitingSpeakers            []SpeakerListItem
+	CurrentInterposedQuestion  *SpeakerListItem
+	WaitingInterposedQuestions []SpeakerListItem
+}
+
+func ListOfSpeakers_CategorizedLists(ctx context.Context, fetch *dsmodels.Fetch, losID int) (ListOfSpeakersLists, error) {
+	lQ := fetch.ListOfSpeakers(losID)
+	los, err := lQ.
+		Preload(lQ.SpeakerList().MeetingUser().StructureLevelList()).
+		Preload(lQ.SpeakerList().MeetingUser().User()).First(ctx)
+	if err != nil {
+		return ListOfSpeakersLists{}, fmt.Errorf("could not load speakers: %w", err)
+	}
+
+	waitingSpeakers := []SpeakerListItem{}
+	interposedQuestions := []SpeakerListItem{}
+	var currentSpeaker *SpeakerListItem
+	var currentInterposedQuestion *SpeakerListItem
+	for _, speaker := range los.SpeakerList {
+		name := ""
+		if meetingUser, isSet := speaker.MeetingUser.Value(); isSet {
+			user := meetingUser.User
+			name = User_ShortName(user)
+			if len(meetingUser.StructureLevelList) != 0 {
+				structureLevelNames := []string{}
+				for _, sl := range meetingUser.StructureLevelList {
+					structureLevelNames = append(structureLevelNames, sl.Name)
+				}
+
+				name = fmt.Sprintf("%s (%s)", name, strings.Join(structureLevelNames, ", "))
+			}
+		}
+
+		item := SpeakerListItem{
+			Name:           name,
+			Weight:         speaker.Weight,
+			IsPointOfOrder: speaker.PointOfOrder,
+			IsContribution: speaker.SpeechState == "contribution",
+			IsIntervention: speaker.SpeechState == "intervention",
+			IsSpeaking:     false,
+		}
+
+		if speaker.BeginTime == 0 && speaker.EndTime == 0 {
+			if speaker.SpeechState == "interposed_question" {
+				interposedQuestions = append(interposedQuestions, item)
+			} else {
+				waitingSpeakers = append(waitingSpeakers, item)
+			}
+		} else if speaker.EndTime == 0 {
+			if speaker.PauseTime == 0 {
+				item.IsSpeaking = true
+			}
+
+			if speaker.SpeechState == "interposed_question" {
+				currentInterposedQuestion = &item
+			} else {
+				currentSpeaker = &item
+			}
+		}
+	}
+
+	sort.Slice(waitingSpeakers, func(i, j int) bool {
+		return waitingSpeakers[i].Weight < waitingSpeakers[j].Weight
+	})
+
+	sort.Slice(interposedQuestions, func(i, j int) bool {
+		return interposedQuestions[i].Weight < interposedQuestions[j].Weight
+	})
+
+	return ListOfSpeakersLists{
+		CurrentSpeaker:             currentSpeaker,
+		WaitingSpeakers:            waitingSpeakers,
+		CurrentInterposedQuestion:  currentInterposedQuestion,
+		WaitingInterposedQuestions: interposedQuestions,
+	}, nil
 }
