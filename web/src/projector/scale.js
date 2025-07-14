901e31adc2175bdb31a8f3cf96369ddc80fe2cfe
Bastian Rihm <brihm@intevation.de>
2025-07-14 15:15:10 +0200

    Add eslint and prettier (#92)

--- web/src/projector/scale.js
+++ web/src/projector/scale.js
@@ -6,16 +6,10 @@ export function setPageWidthVar(shadowDom) {
       return;
     }
 
-    const projectorWidth = +getComputedStyle(container).getPropertyValue(
-      `--projector-width`,
-    );
+    const projectorWidth = +getComputedStyle(container).getPropertyValue(`--projector-width`);
     const projectorAspectRatio =
-      +getComputedStyle(container).getPropertyValue(
-        `--projector-aspect-ratio-denominator`,
-      ) /
-      +getComputedStyle(container).getPropertyValue(
-        `--projector-aspect-ratio-numerator`,
-      );
+      +getComputedStyle(container).getPropertyValue(`--projector-aspect-ratio-denominator`) /
+      +getComputedStyle(container).getPropertyValue(`--projector-aspect-ratio-numerator`);
     const projectorHeight = projectorWidth * projectorAspectRatio;
     const projectorPageAspectRatio = pageEl.offsetHeight / pageEl.offsetWidth;
 
@@ -24,32 +18,29 @@ export function setPageWidthVar(shadowDom) {
       containerWidth = pageEl.offsetHeight / projectorAspectRatio;
     }
 
-    pageEl.style.setProperty("--projector-container-width", `${Math.floor(containerWidth)}`);
-    pageEl.style.setProperty(
-      "--projector-container-height",
-      `${(containerWidth / projectorWidth) * projectorHeight}`,
-    );
+    pageEl.style.setProperty('--projector-container-width', `${Math.floor(containerWidth)}`);
+    pageEl.style.setProperty('--projector-container-height', `${(containerWidth / projectorWidth) * projectorHeight}`);
 
-    pageEl.style.setProperty("--projector-height", `${projectorHeight}`);
+    pageEl.style.setProperty('--projector-height', `${projectorHeight}`);
 
     const headerHeight = shadowDom.querySelector(`#header`) ? 70 : 0;
     const footerHeight = shadowDom.querySelector(`#footer`) ? 35 : 0;
     const innerHeight = projectorHeight - headerHeight - footerHeight;
-    pageEl.style.setProperty("--projector-inner-height", `${innerHeight}`);
+    pageEl.style.setProperty('--projector-inner-height', `${innerHeight}`);
   }
 
-  window.addEventListener("load", update);
-  window.addEventListener("resize", update);
-  pageEl.addEventListener("resize", update);
+  window.addEventListener('load', update);
+  window.addEventListener('resize', update);
+  pageEl.addEventListener('resize', update);
 
   return {
     update() {
       update();
     },
     unregister() {
-      window.removeEventListener("load", update);
-      window.removeEventListener("resize", update);
-      pageEl.removeEventListener("resize", update);
+      window.removeEventListener('load', update);
+      window.removeEventListener('resize', update);
+      pageEl.removeEventListener('resize', update);
     }
-  }
+  };
 }
