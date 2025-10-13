c5df934c7e901d653c1b21581e3e801795b50995
4echow <33332102+4echow@users.noreply.github.com>
2025-10-13 14:41:54 +0200

    Show intervention timer with structure levels (#122)

--- pkg/projector/slide/current_structure_level_list.go
+++ pkg/projector/slide/current_structure_level_list.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/OpenSlides/openslides-go/datastore/dsmodels"
 	"github.com/OpenSlides/openslides-projector-service/pkg/viewmodels"
 )
 
@@ -29,6 +30,7 @@ func CurrentStructureLevelListSlideHandler(ctx context.Context, req *projectionR
 	l := req.Fetch.ListOfSpeakers(*losID)
 	los, err := l.Preload(l.StructureLevelListOfSpeakersList().SpeakerList()).
 		Preload(l.StructureLevelListOfSpeakersList().StructureLevel()).
+		Preload(l.SpeakerList().StructureLevelListOfSpeakers().StructureLevel()).
 		First(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("could not load list of speakers %w", err)
@@ -41,6 +43,7 @@ func CurrentStructureLevelListSlideHandler(ctx context.Context, req *projectionR
 		SpeechState   string  `json:"speech_state"`
 		CountdownTime float64 `json:"remaining_time"`
 		Running       bool    `json:"current_start_time"`
+		Intervention  bool
 	}
 	structureLevels := []structureLevelEntry{}
 	for _, sllos := range los.StructureLevelListOfSpeakersList {
@@ -73,6 +76,48 @@ func CurrentStructureLevelListSlideHandler(ctx context.Context, req *projectionR
 			CountdownTime: countdownTime,
 			Running:       countdownRunning,
 		})
+	}
+
+	var interventionSpeakers []dsmodels.Speaker
+	for _, speaker := range los.SpeakerList {
+		if speaker.SpeechState == "intervention" && speaker.EndTime == 0 {
+			interventionSpeakers = append(interventionSpeakers, speaker)
+		}
+	}
+
+	if len(interventionSpeakers) > 0 {
+		defaultInterventionTime, err := req.Fetch.Meeting_ListOfSpeakersInterventionTime(los.MeetingID).Value(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("could not load intervention time %w", err)
+		}
+		interventionEntry := structureLevelEntry{
+			CountdownTime: float64(defaultInterventionTime),
+			Intervention:  true,
+		}
+
+		var currentInterventionSpeaker *dsmodels.Speaker
+		for _, s := range interventionSpeakers {
+			if viewmodels.Speaker_IsCurrent(&s) {
+				currentInterventionSpeaker = &s
+			}
+		}
+
+		if currentInterventionSpeaker != nil {
+			running := currentInterventionSpeaker.PauseTime == 0
+			interventionEntry.Running = running
+
+			interventionEntry.CountdownTime = viewmodels.Speaker_CalculateInterventionCountdownTime(currentInterventionSpeaker, defaultInterventionTime)
+
+			if currentInterventionSpeaker.StructureLevelListOfSpeakers != nil {
+				sllos, ok := currentInterventionSpeaker.StructureLevelListOfSpeakers.Value()
+				if ok {
+					interventionEntry.ID = sllos.StructureLevelID
+					interventionEntry.Color = sllos.StructureLevel.Color
+				}
+			}
+		}
+
+		structureLevels = append(structureLevels, interventionEntry)
 	}
 
 	titleInfo, err := viewmodels.GetTitleInformationByContentObject(ctx, req.Fetch, los.ContentObjectID)
