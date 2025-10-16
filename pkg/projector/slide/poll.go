5310bef6099597b14e4015bf048b56c3518cb65a
Bastian Rihm <brihm@intevation.de>
2025-10-16 12:30:58 +0200

    Handle non finished poll states

--- pkg/projector/slide/poll.go
+++ pkg/projector/slide/poll.go
@@ -5,25 +5,33 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/OpenSlides/openslides-projector-service/pkg/viewmodels"
+	"github.com/shopspring/decimal"
 )
 
 type pollSlideOptions struct {
 	SingleVotes bool `json:"single_votes"`
 }
 
+type pollSlideProjectionOptionData struct {
+	Color      string
+	Icon       string
+	Name       string
+	TotalVotes decimal.Decimal
+	PercVotes  decimal.Decimal
+}
+
+type pollSlideChartProjectionData struct {
+	TotalValidvotes decimal.Decimal
+	PercValidvotes  decimal.Decimal
+	Options         []pollSlideProjectionOptionData
+}
+
 func PollSlideHandler(ctx context.Context, req *projectionRequest) (map[string]any, error) {
-	options := pollSlideOptions{
-		SingleVotes: false,
-	}
+	var options pollSlideOptions
 	if len(req.Projection.Options) > 0 {
 		if err := json.Unmarshal(req.Projection.Options, &options); err != nil {
 			return nil, fmt.Errorf("could not parse slide options: %w", err)
 		}
-	}
-
-	if options.SingleVotes {
-		return pollSingleVotesSlideHandler(ctx, req)
 	}
 
 	poll, err := req.Fetch.Poll(*req.ContentObjectID).First(ctx)
@@ -31,13 +39,33 @@ func PollSlideHandler(ctx context.Context, req *projectionRequest) (map[string]a
 		return nil, fmt.Errorf("could not load poll id %w", err)
 	}
 
-	title, err := viewmodels.GetContentObjectField[string](ctx, req.Fetch, "title", poll.ContentObjectID)
-	if err != nil {
-		return nil, fmt.Errorf("could not load poll content object title %w", err)
+	if poll.State != "published" && (poll.State != "started" && poll.LiveVotingEnabled) {
+		state := req.Locale.Get("No results yet")
+		if poll.State == "finished" {
+			state = req.Locale.Get("Counting of votes is in progress ...")
+		}
+
+		if poll.State == "started" && !poll.LiveVotingEnabled {
+			state = req.Locale.Get("Voting in progress")
+		}
+
+		return map[string]any{
+			"Title": poll.Title,
+			"State": state,
+		}, nil
 	}
 
+	if options.SingleVotes {
+		return pollSingleVotesSlideHandler(ctx, req)
+	}
+
+	template := "poll_chart"
+	data := pollSlideChartProjectionData{}
+
 	return map[string]any{
-		"Title": title,
-		"Poll":  poll,
+		"_template":   template,
+		"_fullHeight": true,
+		"Poll":        poll,
+		"Data":        data,
 	}, nil
 }
