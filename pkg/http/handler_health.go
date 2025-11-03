ecc34abcf45c0c02651f6e7b7a32fdd6390e5df3
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-11-03 13:28:22 +0100

    Stop laughing

--- pkg/http/handler_health.go
+++ pkg/http/handler_health.go
@@ -2,13 +2,10 @@ package http
 
 import (
 	"net/http"
-
-	"github.com/rs/zerolog/log"
 )
 
 func (s *projectorHttp) HealthHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Info().Msg("lol")
 		w.Header().Set("Content-Type", "application/json")
 		writeResponse(w, `{"healthy": true}`)
 	}
