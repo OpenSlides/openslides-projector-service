22e20a399afe8f7cb0d9a31e4fcebd3e29a9e0a9
Elblinator <lelble@intevation.de>
2025-12-02 15:26:38 +0100

    scroll sideways

--- web/src/slide/projector_motion_block.js
+++ web/src/slide/projector_motion_block.js
@@ -53,14 +53,15 @@ export class ProjectorMotionBlock extends HTMLElement {
     const maxNumberHeight = span.offsetHeight;
 
     const maxGridHeight = this.offsetHeight - 113; // the title is 113px high
-    const numberOfMotionsPerColumn = (maxGridHeight / maxNumberHeight);
+    const numberOfMotionsPerColumn = maxGridHeight / maxNumberHeight;
 
     const neededColumnAmount = Math.ceil(motionNumbers.length / numberOfMotionsPerColumn);
-    
+    const extraColumnWidth = 100 / this.MAX_COLUMNS;
+    const addtionalColumns = (neededColumnAmount * extraColumnWidth).toFixed(0);
+
     const gridContainer = this.querySelector(`.grid-container`);
+    gridContainer.style.setProperty(`--scroll-value`, `${(this.offsetWidth / 100) * extraColumnWidth}px`);
     if (neededColumnAmount > this.MAX_COLUMNS) {
-      const extraColumnWidth = 100 / this.MAX_COLUMNS;
-      const addtionalColumns = (neededColumnAmount * extraColumnWidth).toFixed(0)
       gridContainer.style.setProperty(`width`, `${addtionalColumns}%`);
     }
   }
