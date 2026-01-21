59fd0d4e9defcdf918a244701163ea2dbef6c9ea
Bastian Rihm <brihm@intevation.de>
2026-01-21 11:38:20 +0100

    Use rel-db openslides-go

--- cmd/projectord/main.go
+++ cmd/projectord/main.go
@@ -16,7 +16,6 @@ import (
 	"github.com/OpenSlides/openslides-go/datastore"
 	"github.com/OpenSlides/openslides-go/datastore/flow"
 	"github.com/OpenSlides/openslides-go/environment"
-	"github.com/OpenSlides/openslides-go/redis"
 	"github.com/OpenSlides/openslides-projector-service/pkg/database"
 	projectorHttp "github.com/OpenSlides/openslides-projector-service/pkg/http"
 )
@@ -58,9 +57,7 @@ func run(cfg config) error {
 	ctx := context.Background()
 
 	env := &environment.ForProduction{}
-	messageBus := redis.New(env)
-
-	dsFlow, err := datastore.NewFlowPostgres(env, messageBus)
+	dsFlow, err := datastore.NewFlowPostgres(env)
 	if err != nil {
 		return fmt.Errorf("connecting to datastore: %w", err)
 	}
