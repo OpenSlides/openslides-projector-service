b9fcea44abe1785be8e1af4e1ed417a5a84e8d88
Bastian Rihm <brihm@intevation.de>
2026-02-06 21:23:55 +0100

    Fix single option votes

--- pkg/projector/slide/poll_single_votes.go
+++ pkg/projector/slide/poll_single_votes.go
@@ -356,7 +356,11 @@ func mapUsersToVote(poll *dsmodels.Poll) (map[int]string, error) {
 					}
 				} else {
 					if val, ok := voteValue[strconv.Itoa(pollOption.ID)]; ok {
-						voteMap[uid], _ = val.(string)
+						if strVote, ok := val.(string); ok {
+							voteMap[uid] = strVote;
+						} else if intVote, ok := val.(float64); ok && intVote > 0 {
+							voteMap[uid] = "Y"
+						}
 					}
 				}
 			} else if voteValue, ok := liveVoteEntry.Value.(string); ok && voteValue != "" {
