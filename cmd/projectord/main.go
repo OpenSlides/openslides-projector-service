112cdab9df93ecb318c51e8fd9d2e9a40e93a8d8
Bastian Rihm <brihm@intevation.de>
2026-06-15 13:34:54 +0200

    Add `WaitPostgresAvailable`

--- cmd/projectord/main.go
+++ cmd/projectord/main.go
@@ -60,6 +60,11 @@ func main() {
 func run(cfg config, lookup environment.Environmenter) error {
 	ctx := context.Background()
 
+	err := datastore.WaitPostgresAvailable(lookup)
+	if err != nil {
+		return fmt.Errorf("waiting for database to become ready: %w", err)
+	}
+
 	dsFlow, err := datastore.NewFlowPostgres(lookup)
 	if err != nil {
 		return fmt.Errorf("connecting to datastore: %w", err)
